e40ad987becdf0ec62711e457d7ff693773a9ffb
kosmx <kosmx.mc@gmail.com>
2024-03-16 20:27:38 +0100

    UART dma using circular buffers

---
+++ Src/os/uart_dma.cpp
@@ -0,0 +1,142 @@
+//
+// Created by kosmx on 3/16/24.
+//
+
+#include "uart_dma.h"
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#include <stdexcept>
+
+namespace os {
+
+    std::vector<std::pair<UART_HandleTypeDef *, uart_dma*>> uart_dma_instances{};
+
+
+    uart_dma *uart_dma::getFor(UART_HandleTypeDef *uart, size_t bufferSize) { auto entry = std::find_if(uart_dma_instances.cbegin(), uart_dma_instances.cend(), [=](auto& it) -> bool{
+            return it.first == uart;
+        });
+        if (entry != uart_dma_instances.cend()) {
+            return entry->second;
+        }
+        uart_dma_instances.push_back(std::pair(uart, new uart_dma(uart, bufferSize)));
+        return (uart_dma_instances[uart_dma_instances.size() - 1]).second;
+    }
+
+    uart_dma::uart_dma(UART_HandleTypeDef *huart, size_t bufSize):
+        huart{huart},
+        readSemaphore{xSemaphoreCreateBinary()},
+        writeSemaphore{xSemaphoreCreateBinary()},
+        readMutex(),
+        writeMutex(),
+        recBuffer{new uint8_t [bufSize]},
+        recBufferLen{bufSize} {
+
+        assert(__builtin_popcount(bufSize) == 1);
+
+        HAL_UART_RegisterCallback(huart, HAL_UART_CallbackIDTypeDef::HAL_UART_TX_COMPLETE_CB_ID, [](UART_HandleTypeDef* huart) {
+            auto newTask = pdFALSE;
+            xSemaphoreGiveFromISR(uart_dma::getFor(huart, 0)->writeSemaphore, &newTask);
+        });
+
+        //xSemaphoreGive(readSemaphore);
+    }
+
+    uart_dma::~uart_dma() {
+        delete[] recBuffer;
+    }
+
+    size_t uart_dma::getDptr() const {
+        return recBufferLen - huart->hdmarx->Instance->NDTR;
+    }
+
+    size_t uart_dma::_available() const {
+        int s = getDptr() - (int)recBufferPos;
+        if (s < 0) s += recBufferLen;
+        return s;
+    }
+
+    uint8_t uart_dma::peek(size_t next) const {
+        auto m = readMutex.lock();
+        if (next >= _available()) {
+            throw std::out_of_range("No data to peek");
+        }
+        return recBuffer[(recBufferPos + next) & (recBufferLen - 1)];
+    }
+
+    RingBufferEntryPtr uart_dma::read(size_t amount) {
+        auto m = readMutex.lock();
+        if (amount > _available()) throw std::out_of_range("Not enough data");
+        size_t tmpPos = recBufferPos;
+        recBufferPos = (recBufferPos + amount) & (recBufferLen - 1);
+        return {recBuffer, recBufferLen, tmpPos, amount};
+    }
+
+    size_t uart_dma::available() const {
+        auto m = readMutex.lock();
+        return _available();
+    }
+
+    HAL_StatusTypeDef uart_dma::write(const uint8_t* data, size_t len) {
+        auto l = writeMutex.lock();
+
+        // i don't care if the semaphore is taken or not, but I need it to be taken.
+        xSemaphoreTake(writeSemaphore, 0);
+
+        HAL_UART_Transmit_DMA(huart, data, len);
+
+        xSemaphoreTake(writeSemaphore, portMAX_DELAY);
+
+        return HAL_OK;
+    }
+
+    RingBufferEntryPtr::RingBufferEntryPtr(const uint8_t *buffer, size_t size, size_t pos, size_t data_size):
+    buffer{buffer},
+    sizeMask{size - 1},
+    pos{pos},
+    startPos{pos},
+    dataSize{data_size} {}
+
+    char RingBufferEntryPtr::operator*() {
+        if (pos - startPos < dataSize) {
+            return buffer[pos & sizeMask];
+        } else {
+            throw std::out_of_range{"ringbuffer index"};
+        }
+    }
+
+    uint8_t RingBufferEntryPtr::operator[](size_t pos) {
+        if (this->pos - startPos + pos < dataSize) {
+            return buffer[(this->pos + pos) & sizeMask];
+        } else {
+            throw std::out_of_range{"ringbuffer index"};
+        }
+    }
+
+    RingBufferEntryPtr &RingBufferEntryPtr::operator++() {
+        pos++;
+        return *this;
+    }
+
+    RingBufferEntryPtr RingBufferEntryPtr::operator++(int) {
+        RingBufferEntryPtr tmp = *this;
+        pos++;
+        return tmp;
+    }
+
+    RingBufferEntryPtr &RingBufferEntryPtr::operator--() {
+        pos--;
+        return *this;
+    }
+
+    RingBufferEntryPtr RingBufferEntryPtr::operator--(int) {
+        RingBufferEntryPtr tmp = *this;
+        pos--;
+        return tmp;
+    }
+
+    bool RingBufferEntryPtr::hasNext() const {
+        return pos - startPos + 1 < dataSize;
+    }
+
+} // os
---
+++ Src/os/uart_dma.h
@@ -0,0 +1,107 @@
+//
+// Created by kosmx on 3/16/24.
+//
+
+#ifndef DRONE_FW_UART_DMA_H
+#define DRONE_FW_UART_DMA_H
+
+#include "os/os.h"
+#include "usart.h"
+#include "mutex_wapper.h"
+
+namespace os {
+
+    // something not so fancy
+    /**
+     * Special pointer type with ring-buffer actions
+     * It is a bit more like to Java iterator than C++ pointers.
+     */
+    class RingBufferEntryPtr {
+
+        // The original buffer
+        // must have a size of a power of 2
+        const uint8_t* const buffer;
+
+        // buffer size mask (size - 1)
+        size_t sizeMask;
+        size_t startPos;
+        size_t pos;
+        size_t dataSize;
+
+    public:
+        RingBufferEntryPtr(const uint8_t* buffer, size_t size, size_t pos, size_t data_size);
+        RingBufferEntryPtr(const RingBufferEntryPtr& other) = default; // Default copy ctor
+
+
+        uint8_t operator[](size_t pos);
+
+        char operator*();
+        //RingBufferEntryPtr operator+(size_t increment) const;
+        RingBufferEntryPtr& operator++(); // pre increment
+        RingBufferEntryPtr operator++(int); // post increment
+        RingBufferEntryPtr& operator--(); // pre increment
+        RingBufferEntryPtr operator--(int); // post increment
+
+        [[nodiscard]] bool hasNext() const;
+
+        // now we can do *p++
+
+        // Just to show it's here.
+        bool operator==(const RingBufferEntryPtr& other) const = default; // Bitwise compare is enough for this class
+
+    };
+
+
+    class uart_dma {
+    private:
+        UART_HandleTypeDef * huart;
+        //SemaphoreHandle_t readSemaphore;
+        SemaphoreHandle_t writeSemaphore;
+
+        // UART can do TX without any RX synchronization
+        mutable Mutex readMutex; // mutable: allow synchronized operation from const methods
+        mutable Mutex writeMutex;
+
+        uint8_t* const recBuffer;
+        const size_t recBufferLen;
+        size_t recBufferPos;
+
+        [[nodiscard]] inline size_t getDptr() const;
+
+        uart_dma(UART_HandleTypeDef* huart, size_t bufSize);
+
+        [[nodiscard]] size_t _available() const;
+    public:
+        static uart_dma* getFor(UART_HandleTypeDef * uart, size_t bufferSize);
+
+        /* -------- Read specific functions -------- */
+        [[nodiscard]] size_t available() const;
+
+        /**
+         * Peek into the buffer without reading
+         * @param next peek index (needed to peek the size byte when using for CRSF)
+         * @return next byte
+         */
+        [[nodiscard]] uint8_t peek(size_t next = 0) const;
+
+        /**
+         * Return a pointer to the available data and move buffer ptr by amount.
+         * The data pointed by the ptr will be overwritten after some time, if the data is needed, please copy it.
+         * @param amount data size to get
+         * @return pointer to data inside the buffer
+         */
+        RingBufferEntryPtr read(size_t amount);  // *(uart->read(1));   c:
+
+
+        /* -------- Write specific functions -------- */
+        
+        HAL_StatusTypeDef write(const uint8_t* data, size_t len);
+
+
+        ~uart_dma(); // Not going to be used, but it doesn't hurt
+
+    };
+
+} // os
+
+#endif //DRONE_FW_UART_DMA_H
