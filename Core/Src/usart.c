801cce8691df81c799a17453617c9d6037686ddd
kosmx <kosmx.mc@gmail.com>
2024-03-16 01:56:14 +0100

    Use circular buffer for UART receive

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -315,7 +315,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     hdma_uart10_rx.Init.MemInc = DMA_MINC_ENABLE;
     hdma_uart10_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_uart10_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_uart10_rx.Init.Mode = DMA_NORMAL;
+    hdma_uart10_rx.Init.Mode = DMA_CIRCULAR;
     hdma_uart10_rx.Init.Priority = DMA_PRIORITY_LOW;
     hdma_uart10_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_uart10_rx) != HAL_OK)
