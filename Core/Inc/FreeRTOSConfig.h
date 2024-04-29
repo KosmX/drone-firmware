7afb04d0e4a99da86978afd7ec362472f2a6f365
kosmx <kosmx.mc@gmail.com>
2024-04-29 19:21:37 +0200

    more freertos?

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -117,7 +117,9 @@ to exclude the API function. */
 #define INCLUDE_xTaskGetSchedulerState       1
 #define INCLUDE_xTimerPendFunctionCall       1
 #define INCLUDE_xQueueGetMutexHolder         1
+#define INCLUDE_pcTaskGetTaskName            1
 #define INCLUDE_uxTaskGetStackHighWaterMark  1
+#define INCLUDE_uxTaskGetStackHighWaterMark2 1
 #define INCLUDE_xTaskGetCurrentTaskHandle    1
 #define INCLUDE_eTaskGetState                1
 
